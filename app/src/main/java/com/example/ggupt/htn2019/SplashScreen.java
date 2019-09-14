5a468152558c4971156aacb47eb4a9477d1822bc
G-Gautam <35315739+ggupta24@users.noreply.github.com>
2019-09-13 23:36:43 -0400

    First commit

---
+++ app/src/main/java/com/example/ggupt/htn2019/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.ggupt.htn2019;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(getApplicationContext(),
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
