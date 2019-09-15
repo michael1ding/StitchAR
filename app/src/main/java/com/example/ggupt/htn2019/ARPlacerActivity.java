bdd6f11a8db27b1244f8cb20c90ca0f36e98fb98
G-Gautam <35315739+ggupta24@users.noreply.github.com>
2019-09-15 00:33:48 -0400

    WIP Model

--- app/src/main/java/com/example/ggupt/htn2019/ARPlacerActivity.java
+++ app/src/main/java/com/example/ggupt/htn2019/ARPlacerActivity.java
@@ -2,7 +2,9 @@ package com.example.ggupt.htn2019;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
@@ -24,6 +28,7 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 /**
@@ -38,19 +43,18 @@ public class ARPlacerActivity extends AppCompatActivity {
     private PointerDrawable pointer = new PointerDrawable();
     private boolean isTracking;
     private boolean isHitting;
+    private ModelLoader modelLoader;
 
-//    @RequiresApi(api = VERSION_CODES.N)
+    //    @RequiresApi(api = VERSION_CODES.N)
 //    @Override
 //    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     // CompletableFuture requires api level 24
     // FutureReturnValueIgnored is not valid
+    @RequiresApi(api = VERSION_CODES.N)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        if (!checkIsSupportedDeviceOrFinish(this)) {
-//            return;
-//        }
-
+        modelLoader = new ModelLoader(new WeakReference<>(this));
         setContentView(R.layout.activity_arplacer);
         fragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
@@ -59,67 +63,7 @@ public class ARPlacerActivity extends AppCompatActivity {
             onUpdate();
         });
 
-//        // When you build a Renderable, Sceneform loads its resources in the background while returning
-//        // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
-//        ModelRenderable.builder()
-//                .setSource(this, R.raw.andy)
-//                .build()
-//                .thenAccept(renderable -> andyRenderable = renderable)
-//                .exceptionally(
-//                        throwable -> {
-//                            Toast toast =
-//                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-//                            toast.setGravity(Gravity.CENTER, 0, 0);
-//                            toast.show();
-//                            return null;
-//                        });
-//
-//        arFragment.setOnTapArPlaneListener(
-//                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-//                    if (andyRenderable == null) {
-//                        return;
-//                    }
-//
-//                    // Create the Anchor.
-//                    Anchor anchor = hitResult.createAnchor();
-//                    AnchorNode anchorNode = new AnchorNode(anchor);
-//                    anchorNode.setParent(arFragment.getArSceneView().getScene());
-//
-//                    // Create the transformable andy and add it to the anchor.
-//                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-//                    andy.setParent(anchorNode);
-//                    andy.setRenderable(andyRenderable);
-//                    andy.select();
-//                });
-//    }
-//
-//    /**
-//     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
-//     * on this device.
-//     *
-//     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
-//     *
-//     * <p>Finishes the activity if Sceneform can not run
-//     */
-//    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-//        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
-//            Log.e(TAG, "Sceneform requires Android N or later");
-//            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-//            activity.finish();
-//            return false;
-//        }
-//        String openGlVersionString =
-//                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-//                        .getDeviceConfigurationInfo()
-//                        .getGlEsVersion();
-//        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-//            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-//            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-//                    .show();
-//            activity.finish();
-//            return false;
-//        }
-//        return true;
+        initializeGallery();
     }
     private void onUpdate() {
         boolean trackingChanged = updateTracking();
@@ -171,5 +115,83 @@ public class ARPlacerActivity extends AppCompatActivity {
     private android.graphics.Point getScreenCenter() {
         View vw = findViewById(android.R.id.content);
         return new android.graphics.Point(vw.getWidth()/2, vw.getHeight()/2);
+    }
+
+    @RequiresApi(api = VERSION_CODES.N)
+    private void initializeGallery() {
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+
+        ImageView andy = new ImageView(this);
+        andy.setImageResource(R.drawable.droid_thumb);
+        andy.setContentDescription("andy");
+        andy.setOnClickListener(view ->{addObject(Uri.parse("andy_dance.sfb"));});
+        gallery.addView(andy);
+    }
+    @RequiresApi(api = VERSION_CODES.N)
+    private void addObject(Uri model) {
+        Frame frame = fragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    modelLoader.loadModel(hit.createAnchor(), model);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    public class ModelLoader {
+        private final WeakReference<ARPlacerActivity> owner;
+        private static final String TAG = "ModelLoader";
+
+        ModelLoader(WeakReference<ARPlacerActivity> owner) {
+            this.owner = owner;
+        }
+
+        @RequiresApi(api = VERSION_CODES.N)
+        void loadModel(Anchor anchor, Uri uri) {
+            if (owner.get() == null) {
+                Log.d(TAG, "Activity is null.  Cannot load model.");
+                return;
+            }
+            ModelRenderable.builder()
+                    .setSource(owner.get(), uri)
+                    .build()
+                    .handle((renderable, throwable) -> {
+                        ARPlacerActivity activity = owner.get();
+                        if (activity == null) {
+                            return null;
+                        } else if (throwable != null) {
+                            activity.onException(throwable);
+                        } else {
+                            activity.addNodeToScene(anchor, renderable);
+                        }
+                        return null;
+                    });
+
+            return;
+        }
+    }
+    public void addNodeToScene(Anchor anchor, ModelRenderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+    public void onException(Throwable throwable){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(throwable.getMessage())
+                .setTitle("Codelab error!");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return;
     }
 }
